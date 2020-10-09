ce84a3b422aa1fe810c9c7afdd6b83ace81dbe70
Bill Morrison <bill1.morrison1@gmail.com>
2020-10-08 17:27:26 -0700

    App using the custom keyboard package.

--- KeyboardUIKitAppUsingPackage/ViewController.swift
+++ KeyboardUIKitAppUsingPackage/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import KeyboardPackage
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let keyboardView = Keyboard(frame: CGRect(x: 0, y: 0, width: 0, height: 300))
+        keyboardView.loadKeyboardNib()
+        textField.inputView = keyboardView
+        (keyboardView as Keyboard).delegate = self
+        
     }
-
-
 }
 
+extension ViewController: KeyboardDelegate {
+    
+    func keyWasTapped(character: String) {
+        switch character {
+            case "⏎":
+                textField.resignFirstResponder()
+            default:
+                textField.insertText(character)
+        }
+    }
+}
